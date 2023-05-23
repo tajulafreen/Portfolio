af15f901153e322ec042dbb92f4429af52ed2079
afreen shaik <affushaik9493@gmail.com>
2023-05-24 01:11:29 +0530

    added menu links

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const hambur = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu-section');
+const nav = document.querySelector('.flexcontainer');
 hambur.onclick = function myfunction() {
   menu.classList.toggle('active');
   hambur.classList.toggle('active');
+  nav.classList.toggle('active');
 };
-document.querySelectorAll('.navbar-item').forEach((n) => n.addEventListener('click', () => {
+document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click', () => {
   hambur.classList.remove('active');
   menu.classList.remove('active');
+  nav.classList.remove('active');
 }));
